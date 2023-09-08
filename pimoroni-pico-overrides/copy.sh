13cba0b01928917896be9a1a9d911445044c6182
NewWheelTech <97138790+NewWheelTech@users.noreply.github.com>
2023-09-08 14:24:55 -0400

    Modified Picographics

---
+++ pimoroni-pico-overrides/copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp CMakeLists.txt ../pimoroni-pico/libraries/
+cp micropython-pico.cmake ../pimoroni-pico/micropython/modules/
+cp picographics.c ../pimoroni-pico/micropython/modules/picgrpahics/
+cp picographics.h ../pimoroni-pico/micropython/modules/picgrpahics/
+cp picographics.cpp ../pimoroni-pico/micropython/modules/picgrpahics/
+
+
--- pimoroni-pico-overrides/copy_script.sh
+++ pimoroni-pico-overrides/copy_script.sh
@@ -2,5 +2,8 @@
 
 cp -v CMakeLists.txt ../pimoroni-pico/libraries/
 cp -v micropython-pico.cmake ../pimoroni-pico/micropython/modules/
+cp -v picographics.c ../pimoroni-pico/micropython/modules/picgrpahics/
+cp -v picographics.h ../pimoroni-pico/micropython/modules/picgrpahics/
+cp -v picographics.cpp ../pimoroni-pico/micropython/modules/picgrpahics/
 
 
