1426937c5d574ac2d2bbbedc8754c7a552e1f507
NewWheelTech <97138790+NewWheelTech@users.noreply.github.com>
2023-09-08 14:53:33 -0400

    Fix copy script

--- pimoroni-pico-overrides/copy.sh
+++ pimoroni-pico-overrides/copy.sh
@@ -2,8 +2,8 @@
 
 cp CMakeLists.txt ../pimoroni-pico/libraries/
 cp micropython-pico.cmake ../pimoroni-pico/micropython/modules/
-cp picographics.c ../pimoroni-pico/micropython/modules/picgrpahics/
-cp picographics.h ../pimoroni-pico/micropython/modules/picgrpahics/
-cp picographics.cpp ../pimoroni-pico/micropython/modules/picgrpahics/
+cp picographics.c ../pimoroni-pico/micropython/modules/picographics/
+cp picographics.h ../pimoroni-pico/micropython/modules/picographics/
+cp picographics.cpp ../pimoroni-pico/micropython/modules/picographics/
 
 
