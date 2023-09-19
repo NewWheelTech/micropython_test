d00cf8ffb29ea80c0001e0e8bb3aa5ba0eab104e
NewWheelTech <97138790+NewWheelTech@users.noreply.github.com>
2023-09-18 21:19:16 -0400

    Remove extra breakout boards

--- pimoroni-pico-overrides/copy.sh
+++ pimoroni-pico-overrides/copy.sh
@@ -2,6 +2,7 @@
 
 cp CMakeLists.txt ../pimoroni-pico/libraries/
 cp micropython-pico.cmake ../pimoroni-pico/micropython/modules/
+cp micropython-common.cmake ../pimoroni-pico/micropython/modules/
 cp picographics.c ../pimoroni-pico/micropython/modules/picographics/
 cp picographics.h ../pimoroni-pico/micropython/modules/picographics/
 cp picographics.cpp ../pimoroni-pico/micropython/modules/picographics/
--- pimoroni-pico-overrides/copy_script.sh
+++ pimoroni-pico-overrides/copy_script.sh
@@ -2,6 +2,7 @@
 
 cp -v CMakeLists.txt ../pimoroni-pico/libraries/
 cp -v micropython-pico.cmake ../pimoroni-pico/micropython/modules/
+cp -v micropython-common.cmake ../pimoroni-pico/micropython/modules/
 cp -v picographics.c ../pimoroni-pico/micropython/modules/picographics/
 cp -v picographics.h ../pimoroni-pico/micropython/modules/picographics/
 cp -v picographics.cpp ../pimoroni-pico/micropython/modules/picographics/
